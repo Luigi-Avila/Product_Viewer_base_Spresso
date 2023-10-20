cd2d9dac1dd1778fe82ea0c8a90fb5934743dece
Luigi <l.avila.uvg@gmail.com>
2023-10-20 11:32:50 -0400

    feat: test with problems of Id added

--- app/src/androidTest/java/com/cursosant/productviewerbase/MainActivityTest.kt
+++ app/src/androidTest/java/com/cursosant/productviewerbase/MainActivityTest.kt
@@ -5,11 +5,13 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -73,5 +75,13 @@ class MainActivityTest{
 
         onView(withId(R.id.etNewQuantity))
             .check(matches(withText("1")))
+    }
+
+    @Test
+    fun checkTextField_startQuantity(){
+        onView(allOf(withId(R.id.etNewQuantity), withContentDescription("cantidad")))
+            .check(matches(withText("1")))
+        onView(allOf(withId(R.id.etNewQuantity), withContentDescription("cantidad alterna")))
+            .check(matches(withText("5")))
     }
 }
