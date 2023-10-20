4ccdeb6d9a6339b21fc3f847c3d5b8a9e9492e58
Luigi <l.avila.uvg@gmail.com>
2023-10-20 12:28:30 -0400

    feat: record test added

---
+++ app/src/androidTest/java/com/cursosant/productviewerbase/MainActivitySubLimitTest.kt
@@ -0,0 +1,72 @@
+package com.cursosant.productviewerbase
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivitySubLimitTest {
+
+    @get:Rule
+    val mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivitySubLimitTest() {
+        val appCompatImageButton = onView(
+            allOf(
+                withId(R.id.ibSub),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+
+        val editText = onView(
+            allOf(
+                withId(R.id.etNewQuantity), withText("1"), withContentDescription("cantidad"),
+                withParent(withParent(withId(R.id.tilNewQuantity))),
+                isDisplayed()
+            )
+        )
+        editText.check(matches(withText("1")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
